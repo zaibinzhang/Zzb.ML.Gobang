c2c67cdf924e7b49158b74dfb9543f9e03404fb3
张小狼 <zaibinzhang@qq.com>
2020-09-27 14:31:42 +0800

    1

--- Zzb.ML.Gobang/Zzb.ML.EF/Migrations/20200927063033_CreateName.Designer.cs
+++ Zzb.ML.Gobang/Zzb.ML.EF/Migrations/20200927063033_CreateName.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Zzb.ML.EF;
 
 namespace Zzb.ML.EF.Migrations
 {
     [DbContext(typeof(ZzbContext))]
-    partial class ZzbContextModelSnapshot : ModelSnapshot
+    [Migration("20200927063033_CreateName")]
+    partial class CreateName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Zzb.ML.Gobang/Zzb.ML.EF/Test.cs
+++ Zzb.ML.Gobang/Zzb.ML.EF/Test.cs
@@ -7,5 +7,21 @@ namespace Zzb.ML.EF
         public Guid TestId { get; set; } = Guid.NewGuid();
 
         public string Name { get; set; }
+
+        public string Name1 { get; set; }
+
+        public string Name2 { get; set; }
+
+        public string Name3 { get; set; }
+
+        public string Name4 { get; set; }
+        public string Name5 { get; set; }
+
+        public string Name6 { get; set; }
+
+        public string Name7 { get; set; }
+
+        public string Name8 { get; set; }
+
     }
 }
