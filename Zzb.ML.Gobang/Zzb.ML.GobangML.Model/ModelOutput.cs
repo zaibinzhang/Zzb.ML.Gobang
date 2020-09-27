198a73641caddee1ec1318cc3e2dd3466890b93b
张小狼 <zaibinzhang@qq.com>
2020-09-27 18:04:09 +0800

    1

--- Zzb.ML.Gobang/Zzb.ML.GobangML.Model/ModelOutput.cs
+++ Zzb.ML.Gobang/Zzb.ML.GobangML.Model/ModelOutput.cs
@@ -10,7 +10,7 @@ namespace Zzb_ML_GobangML.Model
         // ColumnName attribute is used to change the column name from
         // its default value, which is the name of the field.
         [ColumnName("PredictedLabel")]
-        public String Prediction { get; set; }
+        public bool Prediction { get; set; }
         public float[] Score { get; set; }
     }
 }
