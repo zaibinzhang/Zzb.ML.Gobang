30d220f083391072b564e11b723838369a57197e
张小狼 <364129572@qq.com>
2020-10-11 16:17:56 +0800

    1

--- Zzb.ML.Gobang/Zzb.ML.Gobang/GameBoard.cs
+++ Zzb.ML.Gobang/Zzb.ML.Gobang/GameBoard.cs
@@ -395,16 +395,21 @@ namespace Zzb.ML.Gobang
         {
             new Task(() =>
             {
-                ModelBuilder.CreateModel();
+                //ModelBuilder.CreateModel();
                 GameBoard board = new GameBoard();
                 MonteCarloTreeSearch.IsWin = (map, point, isBlack) =>
                 {
-                    GameBoard board = new GameBoard();
-                    board.map = map;
-                    board.map[point.Y, point.X] = isBlack ? 1 : 2;
-                    var b = board.IsGameEnd(point);
-                    board.map[point.Y, point.X] = 0;
-                    return b;
+                    lock (board)
+                    {
+                        board.map = map;
+                        board.map[point.Y, point.X] = isBlack ? 1 : 2;
+                        var b = board.IsGameEnd(point);
+                        board.map[point.Y, point.X] = 0;
+
+                        return b;
+
+                    }
+                  
                 };
                 while (true)
                 {
