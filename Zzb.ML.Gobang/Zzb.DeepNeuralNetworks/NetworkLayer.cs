f2d21962e1095165598f58c41894f0a2336d179d
张小狼 <zaibinzhang@qq.com>
2020-11-11 12:43:47 +0800

    向前传播完成

--- Zzb.ML.Gobang/Zzb.DeepNeuralNetworks/NetworkLayer.cs
+++ Zzb.ML.Gobang/Zzb.DeepNeuralNetworks/NetworkLayer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Zzb.DeepNeuralNetworks
 {
@@ -6,14 +7,11 @@ namespace Zzb.DeepNeuralNetworks
     {
         private List<double> _neurons = new List<double>();
 
-        private List<List<double>> _ws = new List<List<double>>();
+        private List<List<double>> _ws;
 
         private NetworkLayer _previousLayer;
 
         private NetworkLayer _nextLayer;
-
-        private double _b = 1;
-
         private void ForwardPropagation()
         {
             if (_nextLayer == null)
@@ -21,7 +19,64 @@ namespace Zzb.DeepNeuralNetworks
                 return;
             }
 
+            CheckWs();
 
+            CalculateNeurons();
+        }
+
+        private double Activation(double d)
+        {
+            return 1 / (1 + Math.Exp(-d));
+        }
+
+        /// <summary>
+        /// 计算神经元
+        /// </summary>
+        private void CalculateNeurons()
+        {
+            for (int i = 0; i < _nextLayer._neurons.Count; i++)
+            {
+                double d = 0;
+                for (int j = 0; j < _neurons.Count; j++)
+                {
+                    d += _neurons[j] * _ws[i][j];
+                }
+
+                d += _ws[i][_neurons.Count];
+
+                _nextLayer._neurons[i] = Activation(d);
+            }
+        }
+
+        /// <summary>
+        /// 检查ws
+        /// </summary>
+        private void CheckWs()
+        {
+            if (_ws == null)
+            {
+                _ws = new List<List<double>>();
+                foreach (var n in _nextLayer._neurons)
+                {
+                    List<double> list = new List<double>();
+                    for (int j = 0; j < _neurons.Count; j++)
+                    {
+                        list.Add(1);
+                    }
+                    _ws.Add(list);
+                }
+
+                //添加偏倚
+                if (_previousLayer != null)
+                {
+                    List<double> list = new List<double>();
+                    for (int j = 0; j < _neurons.Count; j++)
+                    {
+                        list.Add(1);
+                    }
+                    _ws.Add(list);
+                }
+            }
         }
     }
 }
