6a0b9f5389bd73914fac1ed08210533632700f4f
张小狼 <zaibinzhang@qq.com>
2020-11-11 12:00:25 +0800

    1

--- Zzb.ML.Gobang/Zzb.DeepNeuralNetworks/NetworkLayer.cs
+++ Zzb.ML.Gobang/Zzb.DeepNeuralNetworks/NetworkLayer.cs
@@ -4,10 +4,24 @@ namespace Zzb.DeepNeuralNetworks
 {
     public class NetworkLayer
     {
-        private List<Neuron> _neurons = new List<Neuron>();
+        private List<double> _neurons = new List<double>();
 
-        private NetworkLayer _forwardLayer;
+        private List<List<double>> _ws = new List<List<double>>();
+
+        private NetworkLayer _previousLayer;
 
         private NetworkLayer _nextLayer;
+
+        private double _b = 1;
+
+        private void ForwardPropagation()
+        {
+            if (_nextLayer == null)
+            {
+                return;
+            }
+
+
+        }
     }
 }
