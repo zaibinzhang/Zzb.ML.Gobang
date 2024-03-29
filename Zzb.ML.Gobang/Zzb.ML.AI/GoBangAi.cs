bf66db430663b6ea9e9cd1eda825bc9647e3c0c3
张小狼 <zaibinzhang@qq.com>
2024-03-29 17:59:28 +0800

    测试失败

--- Zzb.ML.Gobang/Zzb.ML.AI/GoBangAi.cs
+++ Zzb.ML.Gobang/Zzb.ML.AI/GoBangAi.cs
@@ -1,9 +1,13 @@
 ﻿using System.Drawing;
 using System.Threading.Channels;
-using Keras.Layers;
-using Keras.Models;
-using Keras.Optimizers;
-using Numpy;
+using Tensorflow.Keras;
+using Tensorflow.Keras.Engine;
+using Tensorflow.Keras.Layers;
+using Tensorflow.Keras.Optimizers;
+using Tensorflow.NumPy;
+using Tensorflow.Operations.Initializers;
+using static Tensorflow.Binding;
+using static Tensorflow.KerasApi;
 
 namespace Zzb.ML.AI
 {
@@ -11,33 +15,50 @@ namespace Zzb.ML.AI
     {
         static GoBangAi()
         {
-            _model = new Sequential();
-            _model.Add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
-                activation: "relu",
-                input_shape: (15, 15, 9), padding: "same"));
-            _model.Add(new Conv2D(64, kernel_size: (3, 3).ToTuple(),
-                activation: "relu", padding: "same"));
-            _model.Add(new Conv2D(128, kernel_size: (3, 3).ToTuple(),
-                activation: "relu", padding: "same"));
-            _model.Add(new Conv2D(256, kernel_size: (3, 3).ToTuple(),
-                activation: "relu", padding: "same"));
-            _model.Add(new Conv2D(128, kernel_size: (3, 3).ToTuple(),
-                activation: "relu", padding: "same"));
-            _model.Add(new Conv2D(64, kernel_size: (3, 3).ToTuple(),
-                activation: "relu", padding: "same"));
-            _model.Add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
-                activation: "relu", padding: "same"));
-            _model.Add(new Conv2D(16, kernel_size: (3, 3).ToTuple(),
-                activation: "relu", padding: "same"));
-            _model.Add(new Conv2D(4, kernel_size: (1, 1).ToTuple(),
-                activation: "relu", padding: "same"));
-            _model.Add(new Flatten());
-            _model.Add(new Dense(225, activation: "softmax"));
-            _model.Compile(loss: "categorical_crossentropy",
-                optimizer: new Adam(0.0001F), metrics: new string[] { "accuracy" });
+            var inputs = keras.Input(shape: (15, 15, 9), name: "棋盘qipan");
+            var x1 = keras.layers.Conv2D(32, 3, activation: "relu", padding: "same").Apply(inputs);
+            var x2 = keras.layers.Conv2D(64, 3, activation: "relu", padding: "same").Apply(x1);
+            var x3 = keras.layers.Conv2D(128, 3, activation: "relu", padding: "same").Apply(x2);
+            var x4 = keras.layers.Conv2D(256, 3, activation: "relu", padding: "same").Apply(x3);
+            var x5 = keras.layers.Conv2D(128, 3, activation: "relu", padding: "same").Apply(x4);
+            var x6 = keras.layers.Conv2D(64, 3, activation: "relu", padding: "same").Apply(x5);
+            var x7 = keras.layers.Conv2D(32, 3, activation: "relu", padding: "same").Apply(x6);
+            var x8 = keras.layers.Conv2D(16, 3, activation: "relu", padding: "same").Apply(x7);
+            var x9 = keras.layers.Conv2D(4, 3, activation: "relu", padding: "same").Apply(x8);
+            var x10 = keras.layers.Flatten().Apply(x9);
+            var x11 = keras.layers.Dense(225, activation: "softmax").Apply(x10);
+
+            _model = keras.Model(inputs, x11, name: "gomoku_cnn");
+
+            _model.compile(keras.optimizers.Adam(0.0001f), keras.losses.CategoricalCrossentropy(), new[] { "accuracy" });
+
+            //_model = new Sequential();
+            //_model.add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
+            //    activation: "relu",
+            //    input_shape: (15, 15, 9), padding: "same"));
+            //_model.Add(new Conv2D(64, kernel_size: (3, 3).ToTuple(),
+            //    activation: "relu", padding: "same"));
+            //_model.Add(new Conv2D(128, kernel_size: (3, 3).ToTuple(),
+            //    activation: "relu", padding: "same"));
+            //_model.Add(new Conv2D(256, kernel_size: (3, 3).ToTuple(),
+            //    activation: "relu", padding: "same"));
+            //_model.Add(new Conv2D(128, kernel_size: (3, 3).ToTuple(),
+            //    activation: "relu", padding: "same"));
+            //_model.Add(new Conv2D(64, kernel_size: (3, 3).ToTuple(),
+            //    activation: "relu", padding: "same"));
+            //_model.Add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
+            //    activation: "relu", padding: "same"));
+            //_model.Add(new Conv2D(16, kernel_size: (3, 3).ToTuple(),
+            //    activation: "relu", padding: "same"));
+            //_model.Add(new Conv2D(4, kernel_size: (1, 1).ToTuple(),
+            //    activation: "relu", padding: "same"));
+            //_model.Add(new Flatten());
+            //_model.Add(new Dense(225, activation: "softmax"));
+            //_model.Compile(loss: "categorical_crossentropy",
+            //    optimizer: new Adam(0.0001F), metrics: new string[] { "accuracy" });
         }
 
-        private static readonly Sequential _model;
+        private static readonly IModel _model;
 
         private readonly Random rand = new();
 
@@ -56,12 +77,15 @@ namespace Zzb.ML.AI
 
         public (double loss, double accuracy) Train(List<Point> whiteHistory, List<Point> blackHistory)
         {
-            var (x, y) = LoadRawData(whiteHistory, blackHistory);
-            var h = _model.Fit(x, y,
+            //var (x, y) = LoadRawData(whiteHistory, blackHistory);
+            int numSamples = 100;
+            var inputsData = np.random.randn(numSamples, 15, 15, 9).astype(np.float32);
+            var labelsData = np.random.randint(0, 225, size: numSamples).astype(np.float32);
+            var h = _model.fit(inputsData, labelsData,
                   epochs: 1,
                   verbose: 0);
-            var loss = h.HistoryLogs["loss"].GetValue(0);
-            var accuracy = h.HistoryLogs["accuracy"].GetValue(0);
+            var loss = h.history["loss"][0];
+            var accuracy = h.history["accuracy"][0];
             return ((double)loss, (double)accuracy);
         }
 
@@ -175,150 +199,150 @@ namespace Zzb.ML.AI
 
             }
 
-            var pre = _model.Predict(np.array(arrX), verbose: 0).reshape(-1);
+            var pre = _model.predict(np.array(arrX), verbose: 0).shape;
 
 
             float[,] res = new float[15, 15];
 
-            for (int i = 0; i < 15; i++)
-            {
-                for (int j = 0; j < 15; j++)
-                {
-                    res[i, j] = pre[i * 15 + j].item<float>();
-                }
-            }
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    for (int j = 0; j < 15; j++)
+            //    {
+            //        res[i, j] = pre[i * 15 + j].item<float>();
+            //    }
+            //}
             return res;
         }
 
-        private (NDarray x, NDarray y) LoadRawData(List<Point> whiteHistory, List<Point> blackHistory)
-        {
-            var isBlack = blackHistory.Count > whiteHistory.Count;
-            var totalSize = blackHistory.Count + whiteHistory.Count;
-            int[,,,] arrX = new int[totalSize, 15, 15, 9];
-            int[,] arrY = new int[totalSize, 225];
-            int[,] mapWhite = new int[15, 15];
-            int[,] mapBlack = new int[15, 15];
+        //private (NDarray x, NDarray y) LoadRawData(List<Point> whiteHistory, List<Point> blackHistory)
+        //{
+        //    var isBlack = blackHistory.Count > whiteHistory.Count;
+        //    var totalSize = blackHistory.Count + whiteHistory.Count;
+        //    int[,,,] arrX = new int[totalSize, 15, 15, 9];
+        //    int[,] arrY = new int[totalSize, 225];
+        //    int[,] mapWhite = new int[15, 15];
+        //    int[,] mapBlack = new int[15, 15];
 
-            for (int i = 0; i < blackHistory.Count; i++)
-            {
-                //构建黑色的输入参数
+        //    for (int i = 0; i < blackHistory.Count; i++)
+        //    {
+        //        //构建黑色的输入参数
 
-                //构建历史步数
-                //构建黑色自己最近走过的步数
-                for (int j = 0; j < 3; j++)
-                {
-                    var blackHistCount = i - 1 - j;
-                    if (blackHistCount >= 0)
-                    {
-                        arrX[i * 2, blackHistory[blackHistCount].X, blackHistory[blackHistCount].Y, j] = 1;
-                    }
-                }
-                //构建白色最近走过的步数
-                for (int j = 0; j < 3; j++)
-                {
-                    var whiteHistCount = i - 1 - j;
-                    if (whiteHistCount >= 0)
-                    {
-                        arrX[i * 2, whiteHistory[whiteHistCount].X, whiteHistory[whiteHistCount].Y, j + 3] = 1;
-                    }
-                }
+        //        //构建历史步数
+        //        //构建黑色自己最近走过的步数
+        //        for (int j = 0; j < 3; j++)
+        //        {
+        //            var blackHistCount = i - 1 - j;
+        //            if (blackHistCount >= 0)
+        //            {
+        //                arrX[i * 2, blackHistory[blackHistCount].X, blackHistory[blackHistCount].Y, j] = 1;
+        //            }
+        //        }
+        //        //构建白色最近走过的步数
+        //        for (int j = 0; j < 3; j++)
+        //        {
+        //            var whiteHistCount = i - 1 - j;
+        //            if (whiteHistCount >= 0)
+        //            {
+        //                arrX[i * 2, whiteHistory[whiteHistCount].X, whiteHistory[whiteHistCount].Y, j + 3] = 1;
+        //            }
+        //        }
 
-                //构建黑色目前局面的着点
-                for (int j = 0; j < 15; j++)
-                {
-                    for (int k = 0; k < 15; k++)
-                    {
-                        arrX[i * 2, j, k, 6] = mapBlack[j, k];
-                        if (mapBlack[j, k] == 1)
-                        {
-                            arrY[i * 2, j * 15 + k] = -1;
-                        }
-                    }
-                }
+        //        //构建黑色目前局面的着点
+        //        for (int j = 0; j < 15; j++)
+        //        {
+        //            for (int k = 0; k < 15; k++)
+        //            {
+        //                arrX[i * 2, j, k, 6] = mapBlack[j, k];
+        //                if (mapBlack[j, k] == 1)
+        //                {
+        //                    arrY[i * 2, j * 15 + k] = -1;
+        //                }
+        //            }
+        //        }
 
-                //构建白色目前局面的着点
-                for (int j = 0; j < 15; j++)
-                {
-                    for (int k = 0; k < 15; k++)
-                    {
-                        arrX[i * 2, j, k, 7] = mapWhite[j, k];
-                        if (mapWhite[j, k] == 1)
-                        {
-                            arrY[i * 2, j * 15 + k] = -1;
-                        }
-                    }
-                }
+        //        //构建白色目前局面的着点
+        //        for (int j = 0; j < 15; j++)
+        //        {
+        //            for (int k = 0; k < 15; k++)
+        //            {
+        //                arrX[i * 2, j, k, 7] = mapWhite[j, k];
+        //                if (mapWhite[j, k] == 1)
+        //                {
+        //                    arrY[i * 2, j * 15 + k] = -1;
+        //                }
+        //            }
+        //        }
 
-                //是否先手
-                for (int j = 0; j < 15; j++)
-                {
-                    for (int k = 0; k < 15; k++)
-                    {
-                        arrX[i * 2, j, k, 8] = 1;
-                    }
-                }
+        //        //是否先手
+        //        for (int j = 0; j < 15; j++)
+        //        {
+        //            for (int k = 0; k < 15; k++)
+        //            {
+        //                arrX[i * 2, j, k, 8] = 1;
+        //            }
+        //        }
 
-                arrY[i * 2, blackHistory[i].X * 15 + blackHistory[i].Y] = isBlack ? 1 : -1;
-                mapBlack[blackHistory[i].X, blackHistory[i].Y] = 1;
+        //        arrY[i * 2, blackHistory[i].X * 15 + blackHistory[i].Y] = isBlack ? 1 : -1;
+        //        mapBlack[blackHistory[i].X, blackHistory[i].Y] = 1;
 
-                if (!isBlack)
-                {
-                    //构建白色的输入参数
-                    //构建白色最近走过的步数
-                    for (int j = 0; j < 3; j++)
-                    {
-                        var whiteHistCount = i - 1 - j;
-                        if (whiteHistCount >= 0)
-                        {
-                            arrX[i * 2 + 1, whiteHistory[whiteHistCount].X, whiteHistory[whiteHistCount].Y, j] = 1;
-                        }
-                    }
-                    //构建黑色最近走过的步数
-                    for (int j = 0; j < 3; j++)
-                    {
-                        var blackHistCount = i - 1 - j;
-                        if (blackHistCount >= 0)
-                        {
-                            arrX[i * 2 + 1, blackHistory[blackHistCount].X, blackHistory[blackHistCount].Y, j + 3] = 1;
-                        }
-                    }
+        //        if (!isBlack)
+        //        {
+        //            //构建白色的输入参数
+        //            //构建白色最近走过的步数
+        //            for (int j = 0; j < 3; j++)
+        //            {
+        //                var whiteHistCount = i - 1 - j;
+        //                if (whiteHistCount >= 0)
+        //                {
+        //                    arrX[i * 2 + 1, whiteHistory[whiteHistCount].X, whiteHistory[whiteHistCount].Y, j] = 1;
+        //                }
+        //            }
+        //            //构建黑色最近走过的步数
+        //            for (int j = 0; j < 3; j++)
+        //            {
+        //                var blackHistCount = i - 1 - j;
+        //                if (blackHistCount >= 0)
+        //                {
+        //                    arrX[i * 2 + 1, blackHistory[blackHistCount].X, blackHistory[blackHistCount].Y, j + 3] = 1;
+        //                }
+        //            }
 
-                    //构建白色目前局面的着点
-                    for (int j = 0; j < 15; j++)
-                    {
-                        for (int k = 0; k < 15; k++)
-                        {
-                            arrX[i * 2 + 1, j, k, 6] = mapWhite[j, k];
-                            if (mapWhite[j, k] == 1)
-                            {
-                                arrY[i * 2 + 1, j * 15 + k] = -1;
-                            }
-                        }
-                    }
+        //            //构建白色目前局面的着点
+        //            for (int j = 0; j < 15; j++)
+        //            {
+        //                for (int k = 0; k < 15; k++)
+        //                {
+        //                    arrX[i * 2 + 1, j, k, 6] = mapWhite[j, k];
+        //                    if (mapWhite[j, k] == 1)
+        //                    {
+        //                        arrY[i * 2 + 1, j * 15 + k] = -1;
+        //                    }
+        //                }
+        //            }
 
-                    //构建黑色目前局面的着点
-                    for (int j = 0; j < 15; j++)
-                    {
-                        for (int k = 0; k < 15; k++)
-                        {
-                            arrX[i * 2 + 1, j, k, 7] = mapBlack[j, k];
-                            if (mapBlack[j, k] == 1)
-                            {
-                                arrY[i * 2 + 1, j * 15 + k] = -1;
-                            }
-                        }
-                    }
+        //            //构建黑色目前局面的着点
+        //            for (int j = 0; j < 15; j++)
+        //            {
+        //                for (int k = 0; k < 15; k++)
+        //                {
+        //                    arrX[i * 2 + 1, j, k, 7] = mapBlack[j, k];
+        //                    if (mapBlack[j, k] == 1)
+        //                    {
+        //                        arrY[i * 2 + 1, j * 15 + k] = -1;
+        //                    }
+        //                }
+        //            }
 
-                    arrY[i * 2 + 1, whiteHistory[i].X * 15 + whiteHistory[i].Y] = isBlack ? -1 : 1;
+        //            arrY[i * 2 + 1, whiteHistory[i].X * 15 + whiteHistory[i].Y] = isBlack ? -1 : 1;
 
-                    mapWhite[whiteHistory[i].X, whiteHistory[i].Y] = 1;
-                }
+        //            mapWhite[whiteHistory[i].X, whiteHistory[i].Y] = 1;
+        //        }
 
 
-   
 
-            }
-            return (np.array(arrX), np.array(arrY));
-        }
+
+        //    }
+        //    return (np.array(arrX), np.array(arrY));
+        //}
     }
 }
